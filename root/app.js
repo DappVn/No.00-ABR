106511624b91e03c79429b062dba7825481d6f08
manhito <44461740+manhito@users.noreply.github.com>
2018-10-25 15:36:57 +0700

    Add files via upload

---
+++ root/app.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+	var btn = document.querySelectorAll('p.btn');
+	var ctn = document.querySelectorAll('div.cnt');
+	console.log(ctn);
+	for (var i = 0; i < btn.length; i++) {
+		btn[i].onclick = function(){
+		for (var i = 0; i < btn.length; i++)
+		{btn[i].classList.add('del_btn');}
+		this.nextElementSibling.classList.add('active');}
+	}	
+	var cancel = document.querySelectorAll('div.cancel');
+	for (var i = 0; i < cancel.length; i++) {
+		cancel[i].onclick  = function(){
+		for (var i = 0; i < btn.length; i++) {
+			btn[i].classList.remove('del_btn');
+			ctn[i].classList.remove('active');
+		}		
+	}	
+	}
+},false)
